e27e0ce0db0b0e752ae1285b4df053ff3a8b741d
momoez <armenaot11@gmail.com>
2023-03-10 22:27:21 +0200

    loading dots

--- script.js
+++ script.js
@@ -6,41 +6,53 @@ const chatSendBtn = chatContainer.querySelector('.chat-send-btn');
 let botReplied = true;
 function addChatbotMessage(message, isBot)
 {
-    // create the message element
     const messageElem = document.createElement('div');
     messageElem.classList.add('chat-message');
 
-    if (isBot)
+    if (isBot && !botReplied)
     {
-        // add the message element to the chat container with a delay
+        const loadingElement = document.createElement('div');
+        loadingElement.classList.add('loading-dots');
+
+        const loadingDot1 = document.createElement('span');
+        const loadingDot2 = document.createElement('span');
+        const loadingDot3 = document.createElement('span');
+
+        loadingElement.appendChild(loadingDot1);
+        loadingElement.appendChild(loadingDot2);
+        loadingElement.appendChild(loadingDot3);
+
+        messageElem.appendChild(loadingElement);
+
+        chatMessagesContainer.appendChild(messageElem);
+
+        console.log("waiting response..");
+        
         setTimeout(() =>
         {
-            if (!botReplied)
-            {
-                messageElem.classList.add('bot-message');
-                messageElem.textContent = message;
-                chatMessagesContainer.appendChild(messageElem);
-                botReplied = true;
-                // scroll to the bottom of the chat container after adding the message
-                chatMessagesContainer.scrollTop = chatMessagesContainer.scrollHeight;
-            }
+            console.log("responded");
+            messageElem.classList.add('bot-message');
+            messageElem.textContent = message;
+            chatMessagesContainer.appendChild(messageElem);
+            botReplied = true;
+            chatMessagesContainer.scrollTop = chatMessagesContainer.scrollHeight;
         }, 1500);
         chatMessagesContainer.scrollTop = chatMessagesContainer.scrollHeight;
-    } 
+    }
     else
     {
         if (botReplied)
         {
+            botReplied = false;
             messageElem.classList.add('user-message');
             messageElem.textContent = message;
             chatMessagesContainer.appendChild(messageElem);
-            // scroll to the bottom of the chat container after adding the message
             chatMessagesContainer.scrollTop = chatMessagesContainer.scrollHeight;
-            botReplied = false;
             chatInput.value = '';
         }
     }
 }
+
 
 function respondToMessage(message)
 {
