e04db506295c89d83cdde83ea6ce4a922bafba7e
moez <mohamedmoez852@gmail.com>
2023-06-18 23:28:34 +0300

    another one

--- server.js
+++ server.js
@@ -1,5 +1,34 @@
 // Import required modules
 const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Initialize Express app
+const app = express();
+
+// Set the static files directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Expose the OpenAI API key to the client-side code
+app.get('/api/config', (req, res) =>
+{
+    const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+    res.json({ OPENAI_API_KEY });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () =>
+{
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+/* // Import required modules
+const express = require('express');
 const path = require('path');
 
 // Initialize Express app
@@ -19,4 +48,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () =>
 {
     console.log(`Server is running on http://localhost:${port}`);
-});
+}); */
--- vercel.config.js
+++
@@ -1,7 +0,0 @@
-// vercel.config.js
-module.exports = {
-    rewrites: async () => [
-        { source: "/api/config", destination: "/api/config.js" },
-        { source: "/:path*", destination: "/public/:path*" },
-    ],
-};
