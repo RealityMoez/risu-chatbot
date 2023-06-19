9baf76b9045a3e086148a2f418e74a513c7f0cb7
moez <mohamedmoez852@gmail.com>
2023-06-19 07:23:39 +0300

    n/a

--- api/get_data.js
+++ api/get_data.js
@@ -5,10 +5,16 @@ module.exports = async (req, res) =>
     try
     {
         const API_KEY = process.env.API_KEY;
+        if(!API_KEY)
+        {
+            throw new Error('API_KEY not found in environment variables');
+        }
+        res.setHeader('Content-Type', 'application/json');
         res.status(200).json({ apiKey: API_KEY });
     } catch(error)
     {
         console.error('Error fetching API key:', error);
+        res.setHeader('Content-Type', 'application/json');
         res.status(500).json({ error: 'Failed to fetch API key' });
     }
 };
